6b93fb43eb990dd3eaea8f025baff975583d1783
Tristan.K <randomdestiny3006@gmail.com>
2024-09-16 19:13:29 -0500

    Add files via upload

--- hw4pr5.py
+++ hw4pr5.py
@@ -4,30 +4,43 @@
 #
 # Task(s): Creating the int8 class!
 
-class int8:
-    num = 0
+from math import*
 
-    
-    def __add__(self, obj):   # self refers to the object that calls the method
+
+class int8: 
+   num = 0
+   king = 1
+   derp = 2
+   obj1 = 2
+   obj2 = 298
+   obj3 = 1
+   
+   def __add__(self, obj):   # self refers to the object that calls the method
        """ this overloads the __add__ method so that Python
            knows how to apply it to int8-objects
        """
        objOut = int8() # creating the int8 object that will be returned
        objOut.num = self.num + obj.num # updating the data-member, num
        return objOut
-    
-    def mystery(val): # this isn't a method that is pre-existing in Python
-       """ A quick example of a static method """
-       objOut = int8()
-       objOut.num = val
-       return objOut
-    
-    def int2int8(self):
+   
+   def mystery(val): # this isn't a method that is pre-existing in Python
+            """ A quick example of a static method """
+            objOut = int8()
+            objOut.num = val
+            return objOut
+   
+   def int2int8(self):
        '''
+       cuts off at 8 bits and leaves the crumbs
        '''
        return self & 255
-    
-    def setVal(self,val):
-       self.num = int8.int2int8(val)
-       return 
+   
+   def setVal(self,val):
+        '''
+        '''
+        self.num = int8.int2int8(val)
+        return  
 
+        
+   
+   
